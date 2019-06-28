40a72ef94eaaea487188caa0f29488ae83ce2b8a
Mladen Petrov <mladen.petrov88@gmail.com>
2019-06-29 01:05:38 +0300

    Add files via upload

---
+++ SpecialNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int x1 = 1; x1 <= 9; x1++) {
+            for (int x2 = 1; x2 <= 9; x2++) {
+                for (int x3 = 1; x3 <= 9; x3++) {
+                    for (int x4 = 1; x4 <= 9; x4++) {
+                        if (n % x1 == 0 && n % x2 == 0 && n % x3 == 0 && n % x4 == 0){
+                            System.out.printf("%d%d%d%d ", x1, x2 ,x3 ,x4);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
